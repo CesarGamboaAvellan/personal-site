ec83cbc82b04e22d21c81ceee3e634b65a84c0df
cesar <cesar@firstfactory.com>
2018-09-07 17:57:07 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -4,43 +4,54 @@ import linkedin from "./assets/siteImages/linkedin.png";
 import github from "./assets/siteImages/github.png";
 import "./App.css";
 import Description from './Description';
+import Contact from './Contact';
 import Technologies from './Technologies';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="div-father">
-          <ul>
-            <li>
-              <a class="active" href="#home">
-                About me
-              </a>
-            </li>
-            <li>
-              <a href="#news">Experience</a>
-            </li>
-            <li>
-              <a href="#contact">Portfolio</a>
-            </li>
-            <li>
-              <a href="#about">Contact</a>
-            </li>
-          </ul>
-            <h2 className="divider"></h2>
-          <img
+    const homePage = ( <div className="div-father">
+        <img
             src={profilePicture}
             alt="profilePicture"
             className="rounder-div"
           />
           <Description />
-          <h2 className="divider"></h2>
-          <Technologies />
+      </div>);
+    return (
+      <div className="App">
+        <div className="div-father">
+          <ul>
+            <li>
+            <Link to = "/">Home</Link>
+            </li>
+            <li>
+            <Link to = "/experience">Job Experience</Link>
+            </li>
+            <li>
+              <Link to = "/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to = "/contact">Contact</Link>
+            </li>
+          </ul>
+            <h2 className="divider"></h2>
         </div>
         <div class="footer">
           <img src={linkedin} alt="linkedin"></img>
           <img src={github} alt="github"></img>
         </div>
+        <Switch>
+      <Route exact path="/">{homePage}</Route>
+      <Route path="/portfolio" component={Contact}/>
+      <Route path="/experience" component={Contact}/>
+      <Route path="/contact" component={Contact}/>
+  </Switch>
       </div>
     );
   }
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const descriptionText = () => (<h4 className="font-style">
+    <p>
+    Hi! i'm Cesar, a born and raised Costa Rican. I've been working as a full stack developer
+    since june 2018 at First Factory, before that, i was an academy developer as part of an intensive 
+    code program. Also, during 2017, i worked as a freelancer developer to get involved 
+    in the amazing world of coding.
+    </p>
+    <p>
+    I learned Math, Physics and logic at UCR when studying Mechanical Engineering, 
+    and currently i'm a systems engineering student at UNA.
+    </p>
+</h4>);
+export default descriptionText;
